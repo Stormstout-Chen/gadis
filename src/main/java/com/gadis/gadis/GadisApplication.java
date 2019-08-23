ad62e3220463749119f50791f9782555989e0558
Gitdatsanvich <tc704321764@outlook.com>
2019-08-23 14:30:53 +0800

    <测试提交>

--- src/main/java/com/gadis/gadis/GadisApplication.java
+++ src/main/java/com/gadis/gadis/GadisApplication.java
@@ -7,7 +7,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class GadisApplication {
-
+    /**
+     * 测试提交
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(GadisApplication.class, args);
     }

c8cea5f173c7ef2e7bf8bead17a6c3e265327a32
Gitdatsanvich <tc704321764@outlook.com>
2019-08-23 15:05:54 +0800

    <更改配置文件由properties改为Yml> <删除测试提交代码>

--- src/main/java/com/gadis/gadis/GadisApplication.java
+++ src/main/java/com/gadis/gadis/GadisApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author Stormstout-Chen
+ */
 @SpringBootApplication
 @EnableScheduling
 public class GadisApplication {
-    /**
-     * 测试提交
-     * @param args
-     */
+
     public static void main(String[] args) {
         SpringApplication.run(GadisApplication.class, args);
     }
